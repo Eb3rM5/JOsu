3967924eff748ba44127e77aebf89ad5718b465a
Éber Mainardes dos Santos <manoebermainardes@gmail.com>
2019-06-29 13:55:50 -0300

    Adding basic method to read lines from an input stream
    I was going to keep this just on the BeatmapTest class, but I know I'll use this in other places too.

--- src/com/eber/josu/util/Util.java
+++ src/com/eber/josu/util/Util.java
@@ -1,5 +1,12 @@
 package com.eber.josu.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Util {
 
 	public static final <T> T cast(Object obj, Class<T> type){
@@ -14,6 +21,22 @@ public final class Util {
 		}
 		
 		return null;
+		
+	}
+	
+	public static final List<String> readFrom(InputStream in) throws IOException{
+		
+		List<String> lines = new ArrayList<>();
+		
+		BufferedReader r = new BufferedReader(new InputStreamReader(in));
+		
+		String l;
+		while ((l = r.readLine()) != null) lines.add(l);
+		
+		r.close();
+		in.close();
+		
+		return lines;
 		
 	}
 	
